43aec8e0cfee12e7dcce25c222c01539d745ff70
Zhyrgalbek kyzy Zhannat <zhannat.zhyrgalbek.on@gmail.com>
2022-01-16 15:52:44 +0600

    add.task5

--- js/script5.js
+++ js/script5.js
@@ -1,6 +1,8 @@
 let cardNum;
 cardNum = prompt('Введите номер вашей карты');
-let number = '1234567899876543';
-number = ( number.replace(number.slice (0 , -4), "**********"));
+
+number = cardNum.replace(cardNum.slice (0 , -4), "************");
+
 alert ('Номер вашей карты' + number);
-console.log("Номер вашей карты: " + number.replace(number.slice (0 , -4), "**********"));
+
+// console.log("Номер вашей карты: " + number.replace(number.slice (0 , -4), "**********"));
